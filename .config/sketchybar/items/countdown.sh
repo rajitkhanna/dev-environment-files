c1ac0bab156385a543ce4e74be71b7fa8b7cc148
rajitkhanna <rajitskhanna@gmail.com>
2025-06-14 10:18:04 -0700

    update sketchybarr

--- .config/sketchybar/items/countdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sketchybar --add item countdown right \
-           --set countdown update_freq=3600 \
-                         script="$PLUGIN_DIR/countdown.sh" \
-           --subscribe countdown system_woke
-
